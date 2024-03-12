01ab0b31e8e29c92af56436334972beee5845013
tzikheng <tzikheng@gmail.com>
2024-03-13 00:11:56 +0800

    [server] cmd/api, /stream, /url

--- server/scripts/init.sh
+++ server/scripts/init.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-# Init app as a docker image, using remote services
-
-APP="fd-server"
-TAG="1"
-PORTS="8094:8080"
-BUILD_PATH="."
-
-# Build app image if not exists
-if [[ "$(docker images -q $APP:$TAG 2> /dev/null)" == "" ]]; 
-  then docker build $BUILD_PATH -t $APP:$TAG
+# Build app image for API service if not exists
+APP_API="fd-service-api"
+TAG_API="1_api"
+if [[ "$(docker images -q $APP_API:$TAG_API 2> /dev/null)" == "" ]]; 
+  then docker build . -t $APP_API:$TAG_API --build-arg service=api
 fi
 
-docker run --name $APP -p $PORTS -e APP_ID=$APP -d $APP:$TAG
+docker run --name $APP_API-1 -p "8094:8080" -d $APP_API:$TAG_API
+
+# Build app image for Stream service if not exists
+APP_STR="fd-service-stream"
+TAG_STR="1_str"
+if [[ "$(docker images -q $APP_STR:$TAG_STR 2> /dev/null)" == "" ]]; 
+  then docker build . -t $APP_STR:$TAG_STR --build-arg service=stream
+fi
+
+docker run --name $APP_STR-1 -p "8095:8080" -d $APP_STR:$TAG_STR
+
+# Build app image for URL service if not exists
+APP_URL="fd-service-url"
+TAG_URL="1_url"
+if [[ "$(docker images -q $APP_URL:$TAG_URL 2> /dev/null)" == "" ]];
+  then docker build . -t $APP_URL:$TAG_URL --build-arg service=url
+fi
+
+docker run --name $APP_URL-1 -p "8096:8080" -d $APP_URL:$TAG_URL
+docker run --name $APP_URL-2 -p "8097:8080" -d $APP_URL:$TAG_URL
